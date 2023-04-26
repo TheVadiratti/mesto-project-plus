6fff93bc4cfb42b704e9a0f2c1b0095dbdb80ae7
Vadim Belousov <thevadiratti@yandex.ru>
2023-04-26 12:39:12 +0500

    feat: hardcode test user

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  req.body.user = {
+    _id: '6448ce5b7272030c477f5ce1',
+  };
+
+  next();
+});
+
 app.use('/', userRouter);
 
 app.listen(3000);
