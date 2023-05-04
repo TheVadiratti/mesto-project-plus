866f78d848f9d34dea6b3f2a9333915ea8b8428b
Vadim Belousov <thevadiratti@yandex.ru>
2023-05-04 15:10:19 +0500

    feat: add loggers

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import usersRouter from './routes/users';
 import cardsRouter from './routes/cards';
 import auth from './middlewares/auth';
 import { login, createUser } from './controllers/users';
+import { requestLogger, errorLogger } from './middlewares/logger';
 
 const app = express();
 
@@ -13,6 +14,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.post('/signin', login);
 app.post('/signup', createUser);
 
@@ -20,6 +23,8 @@ app.use(auth);
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+
+app.use(errorLogger);
 
 app.use(errors);
 
