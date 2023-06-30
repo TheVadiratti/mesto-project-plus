96fde2cdb496c71d37f8e1f71308a0debe80c77e
Vadim Belousov <thevadiratti@yandex.ru>
2023-06-30 11:09:05 +0500

    feat: добавить ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'mesto-api',
+    script: 'dist/app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:TheVadiratti/mesto-project-plus.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm install && npm run build',
+    },
+  },
+};
